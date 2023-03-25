44217ac08b6c065e5240512587f199c2db296e9d
Hossam <56669856+HOSS11H@users.noreply.github.com>
2023-03-25 07:05:37 +0200

    pre render all events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,14 +1,12 @@
 import { useRouter } from 'next/router';
 import EventsList from "../../components/Events/EventsList/EventsList";
 import EventsSearch from "../../components/Events/EventsSearch/EventsSearch";
-import { getAllEvents } from "../../dummy-data";
+import { getAllEvents } from '../../helpers/api-utils';
 
-const Events = ( ) => {
+const Events = ( props ) => {
 
     const router = useRouter()
     const { push } = router
-
-    const events = getAllEvents()
 
     const findEventsHandler = ( year, month ) => {
         push({
@@ -22,8 +20,21 @@ const Events = ( ) => {
     return (
         <div>
             <EventsSearch onSearch={findEventsHandler} />
-            <EventsList items={events} />
+            <EventsList items={props.events} />
         </div>
     )
 }
 export default Events;
+
+export async function getStaticProps() {
+
+    const events = await getAllEvents()
+
+    return {
+        props: {
+            events,
+        },
+        // it revalidates the page every 30 seconds on The Server even after the page is built
+        revalidate: 30,
+    }
+}
