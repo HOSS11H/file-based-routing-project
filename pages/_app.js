2c9104979b7c02c98f177a8812baa868fb37cb21
Hossam <56669856+HOSS11H@users.noreply.github.com>
2023-03-27 06:26:38 +0200

    add theme color meta

--- pages/_app.js
+++ pages/_app.js
@@ -21,6 +21,7 @@ function MyApp({ Component, pageProps }) {
         <Layout>
           <Head>
             <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+            <meta name='theme-color' content='#000000' />
           </Head>
           <Component {...pageProps} />
         </Layout>
